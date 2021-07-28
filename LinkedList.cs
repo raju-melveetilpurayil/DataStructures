425717ea41766586841731f5bb9a4594f892d6f0
Raju Melveetilpurayil <raju.m@live.com>
2021-07-28 12:28:29 +0100

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class LinkedList<T>
+    {
+        private Node<T> head { get; set; }
+        private int length { get; set; }
+        public bool Search(Node<T> list,T search)
+        {
+            bool found = false;
+            while (!found)
+            {
+                if (list.Data.ToString() == search.ToString())
+                {
+                    found = true;
+                }
+                else
+                {
+                    Search(list.Next, search);
+                }
+            }
+            return found;
+        }
+        public void Add(T item)
+        {
+            Node<T> newNode = new Node<T>(item);
+            Node<T> current = head;
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+    }
+   
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; } = null;
+        public Node(T data, Node<T> next = null)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 4, 3, 5, 7, 9 };
+            LinkedList<int> linkedList = new LinkedList<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                linkedList.Add(numbers[i]);
+            }
+        }
+    }
+}
